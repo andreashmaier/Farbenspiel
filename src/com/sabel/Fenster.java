2e09d244f7490b6d0949c3f68c96725e4b2964dc
Andreas Maier <maier.gbs@sabel.com>
2017-09-21 10:06:58 +0200

    Eventhandling in an inner class MeinInnererActionListener

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,6 +1,9 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Fenster extends JFrame {
     private JPanel jPanel;
@@ -12,8 +15,16 @@ public class Fenster extends JFrame {
         super("Farbenspiel");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.initComponents();
+        this.initEvents();
         this.setSize(300, 300);
         this.setVisible(true);
+    }
+
+    private void initEvents() {
+        MeinInnererActionListener mial = new MeinInnererActionListener();
+        jbtnRot.addActionListener(mial);
+        jbtnBlau.addActionListener(mial);
+        jbtnGelb.addActionListener(mial);
     }
 
 
@@ -33,4 +44,22 @@ public class Fenster extends JFrame {
     }
 
 
+    public class MeinInnererActionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(e.getActionCommand());
+            switch (e.getActionCommand()){
+                case "Rot":
+                    jPanel.setBackground(Color.RED);
+                    break;
+                case "Gelb":
+                    jPanel.setBackground(Color.YELLOW);
+                    break;
+                case "Blau":
+                    jPanel.setBackground(Color.BLUE);
+                    break;
+            }
+        }
+    }
 }
