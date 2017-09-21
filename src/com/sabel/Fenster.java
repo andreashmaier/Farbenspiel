28c8c420cb88b8fe0ca9664bad045d105abed12e
Andreas Maier <maier.gbs@sabel.com>
2017-09-21 10:16:58 +0200

    Eventhandling with anonymous inner class

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -1,6 +1,9 @@
 package com.sabel;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Fenster extends JFrame {
     private JPanel jPanel;
@@ -12,8 +15,32 @@ public class Fenster extends JFrame {
         super("Farbenspiel");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.initComponents();
+        this.initEvents();
         this.setSize(300, 300);
         this.setVisible(true);
+    }
+
+    private void initEvents() {
+        jbtnRot.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.RED);
+            }
+        });
+
+        jbtnGelb.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.YELLOW);
+            }
+        });
+
+        jbtnBlau.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jPanel.setBackground(Color.BLUE);
+            }
+        });
     }
 
 
